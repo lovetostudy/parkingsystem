2db0d26ac4f2bfd18d081a817bde0161969307a5
kai <kai137497294@gmail.com>
2018-08-07 17:04:30 +0800

    update some layouts

---
+++ app/src/main/java/com/parkingsystem/activity/TopupActivity.java
@@ -0,0 +1,16 @@
+package com.parkingsystem.activity;
+
+import android.os.Bundle;
+
+import com.parkingsystem.R;
+
+public class TopupActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_topup);
+
+
+    }
+}
